1f6f20dd58887ba165a45db31506f4159370d2d6
redaktteedd <novantomichael1@gmail.com>
2025-10-06 18:02:20 +1100

    Add placeholder puzzle game for future development

---
+++ renderer/common/puzzles.ts
@@ -0,0 +1,36 @@
+export interface File{
+    name: string,
+    content: string
+}
+
+export interface Puzzle{
+    description: string,
+    files: File[]
+    solution: string[]
+}
+
+//Function to load a file from the assets/puzzles directory
+export async function loadFile(filename: string): Promise<File>{
+    try {
+        const response = await fetch(`../../assets/puzzles/${filename}`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.text();
+        return {name: filename, content: data};
+    } catch (error) {
+        console.error('Error fetching the puzzle:', error);
+        throw error;
+    }
+}
+
+//Function to create a puzzle
+export async function loadReadPuzzle(): Promise<Puzzle>{
+    const files = await Promise.all([loadFile("clue1.txt"), loadFile("clue2.txt"), loadFile("clue3.txt")]);
+    return {
+        description: "Read the files to find the secret code.",
+        files: files,
+        solution: ["cat clue1.txt", "cat clue2.txt", "cat clue3.txt"]
+    };
+    
+}
