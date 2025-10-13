f0cb3ca135dc7d5c65834823ad58651ea8e56f04
michaelntk <novantomichael1@gmail.com>
2025-10-14 00:23:50 +1100

    Add a method to format output

---
+++ renderer/common/helper.ts
@@ -0,0 +1,8 @@
+/**
+ * Function to format for terminal output
+ * @param lines Array of strings to format
+ * @returns formatted string with new lines
+ */
+export function formatOutput(lines: string[]): string {
+    return lines.join('\n');
+}
